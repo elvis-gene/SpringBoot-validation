e21646f16ecfe5207d0ddc8eef620bc1f2c4a30d
Elvis Gene <elvis.pgene@gmail.com>
2020-09-11 22:31:15 +0200

    Initial commit

---
+++ src/main/java/com/example/pringbootvalidation/PringBootValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.pringbootvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringBootValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PringBootValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pringbootvalidation/PringBootValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pringbootvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PringBootValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
